a124ac3c52a805a3a606f72be99e3a9b78a2c66b
Carlos Torres <18730251+andytheandroid@users.noreply.github.com>
2024-05-21 17:47:15 -0600

    created the MenuInventory, Ingredient and Purchase Models
    Created the models for the inventory app and performed the required migrations

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)  # Add a name field
+    qty = models.DecimalField(max_digits=10, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuInventoryItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Purchase(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount_purchased = models.DecimalField(max_digits=10, decimal_places=2)
+    purchase_date = models.DateTimeField(auto_now_add=True)  # Add a date field
+
+    @property
+    def total(self):
+        return self.amount_purchased * self.ingredient.unit_price  # Calculate total
+
+    def __str__(self):
+        return f"{self.amount_purchased} of {self.ingredient.name}"
